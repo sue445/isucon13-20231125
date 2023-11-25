97c6ca1ec45cf18c04f42cde5166ba08ce5ad6bb
sue445 <sue445@sue445.net>
2023-11-25 15:35:24 +0900

    GET /api/user/:username/statistics : Add index to livecomments
    Close #15

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -1,7 +1,9 @@
 -- FIXME: truncateだとindexが消えないので冪等性を保つために手動で投入
 
-ALTER TABLE themes ADD INDEX user_id (user_id);
+ALTER TABLE livecomments ADD INDEX livestream_id (livestream_id);
 
 ALTER TABLE livestream_tags ADD INDEX livestream_id (livestream_id);
 
 ALTER TABLE reactions ADD INDEX livestream_id_and_created_at (livestream_id, created_at);
+
+ALTER TABLE themes ADD INDEX user_id (user_id);
